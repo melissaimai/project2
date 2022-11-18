1ee0653e226dbbf00da40142770def72c216b9f3
sijincodes <sijinmathews03@gmail.com>
2022-11-18 16:07:54 +0100

    Added calendar skeleton

--- Sticky-to-make/routes/index.routes.js
+++ Sticky-to-make/routes/index.routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("initial");
 });
 
 module.exports = router;
