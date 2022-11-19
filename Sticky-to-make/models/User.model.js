c6f93177e9cdb4adba18b37e16d4679f9296eac6
sijincodes <sijinmathews03@gmail.com>
2022-11-19 14:34:08 +0100

    Login Feature - Login API Created - Login Handlebar file created - Removed email from user model - Updated Signup api to call login page - Few small updates on the sign up api

--- Sticky-to-make/models/User.model.js
+++ Sticky-to-make/models/User.model.js
@@ -6,24 +6,17 @@ const userSchema = new Schema(
     username: {
       type: String,
       trim: true,
-      required: false,
-      unique: true
-    },
-    email: {
-      type: String,
       required: true,
       unique: true,
-      lowercase: true,
-      trim: true
     },
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
