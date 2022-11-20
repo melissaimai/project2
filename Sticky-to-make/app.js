f3990dceef897580082d2b0f1fdd36bf780b44da
Ekta <ekta.cca@gmail.com>
2022-11-20 19:39:12 +0100

    styling changed -initial page

--- Sticky-to-make/app.js
+++ Sticky-to-make/app.js
@@ -48,6 +48,9 @@ app.use('/', index);
 const authRoutes = require("./routes/auth.routes");
 app.use('/', authRoutes);
 
+const initial = require("./routes/initial.routes");
+app.use('/', initial);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
---
+++ Sticky-to-make/routes/initial.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page */
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+router.get("/initial", (req, res, next) => {
+  res.render("initial");
+});
+
+module.exports = router;
